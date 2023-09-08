bc18ce0fe117e6f16df21b80b326eebddee3c36b
Kopta <59294990+isaacadejoro@users.noreply.github.com>
2023-09-08 22:01:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-// JavaScript code to update real-time elements
-const updateRealTimeElements = () => {
-    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const now = new Date();
+
+let time = document.querySelector(`[data-testid="currentUTCTime"]`);
+
+setInterval(() => {
+  const d = Date.now();
+  time.innerHTML = d;
+}, 100);
+
+    function updateDayOfWeek() {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+        const currentDate = new Date();
+        const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+        currentDayElement.textContent = currentDayOfWeek;
+    }
+    updateDayOfWeek();
     
-    dayOfWeekElement.textContent = daysOfWeek[now.getUTCDay()];
-    utcTimeElement.textContent = now.toISOString().substr(11, 8) + " UTC";
-};
-
-// Update real-time elements every second
-setInterval(updateRealTimeElements, 1000);
-
